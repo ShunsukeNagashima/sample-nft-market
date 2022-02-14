75c9c02647080898fad5df3374b3b9a85765b8a8
Shunsuke Nagashima <42495880+ShunsukeNagashima@users.noreply.github.com>
2022-02-15 02:24:58 +0900

    initial setup

---
+++ hardhat.config.js
@@ -0,0 +1,26 @@
+require('@nomiclabs/hardhat-waffle');
+
+const projectId = '2e0801dfeead43e69039a5fc4f2b3119';
+const privateKey = process.env.PRIVATE_KEY;
+
+module.exports = {
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {
+      chainId: 1337, // config standard
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey],
+    },
+  },
+  solidity: {
+    version: '0.8.9',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
