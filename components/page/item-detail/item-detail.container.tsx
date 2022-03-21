14076d58780b27c485a7d6fd2d77aff8bc44d350
Shunsuke Nagashima <42495880+ShunsukeNagashima@users.noreply.github.com>
2022-03-21 15:00:59 +0900

    fix: way to fetch all tokens and refactor some codes

--- components/page/item-detail/item-detail.container.tsx
+++ components/page/item-detail/item-detail.container.tsx
@@ -1,41 +1,30 @@
 import { useState, useEffect, useCallback } from 'react';
 import { ItemDetailComponent } from './item-detail';
 import { useRouter } from 'next/router';
-import { ethers } from 'ethers';
-import axios from 'axios';
 
 import { INFT } from '../nft-list/types';
-import { nftaddress, nftmarketaddress } from '../../../config';
-import Market from '../../../artifacts/contracts/Market.sol/Market.json';
-import NFT from '../../../artifacts/contracts/NFT.sol/NFT.json';
+import { fetchTokenById } from '../../../utils/contractHelper';
 
 export const ItemDetail = () => {
   const [loadedNft, setLoadedNft] = useState<INFT>();
   const router = useRouter();
 
+  const getAsString = (value: string | string[]) => {
+    if (Array.isArray(value)) {
+      return value[0];
+    }
+
+    return value;
+  };
+
   const loadNFTByTokenId = useCallback(async () => {
     const { tokenId } = router.query;
     if (!tokenId) return;
-    console.log(tokenId);
-    const provider = new ethers.providers.JsonRpcProvider();
-    const marketContract = new ethers.Contract(nftmarketaddress, Market.abi, provider);
-    const nftContract = new ethers.Contract(nftaddress, NFT.abi, provider);
-    const token = await marketContract.fetchTokenByTokenId(1);
-    const tokenUri = await nftContract.tokenURI(tokenId);
-    const meta = await axios.get(tokenUri);
-    const priceInEther = ethers.utils.formatUnits(token.price.toString(), 'ether');
-    const item = {
-      itemId: parseInt(token.itemId),
-      tokenId: parseInt(tokenId),
-      priceInEther,
-      seller: token.seller,
-      owner: token.owner,
-      name: meta.data.name,
-      description: meta.data.description,
-      image: meta.data.image,
-    };
+    const item = await fetchTokenById(parseInt(getAsString(tokenId)));
+    if (!item) {
+      throw new Error('Something went wrong, please try again.');
+    }
     setLoadedNft(item);
-    console.log(item);
   }, [router.query]);
 
   useEffect(() => {
