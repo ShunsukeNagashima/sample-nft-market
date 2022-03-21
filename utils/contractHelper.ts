adc6c7164f96628bf2db86adad936192a4547f89
Shunsuke Nagashima <42495880+ShunsukeNagashima@users.noreply.github.com>
2022-03-21 23:50:12 +0900

    feat: add helper func to fetch owned tokens

--- utils/contractHelper.ts
+++ utils/contractHelper.ts
@@ -60,3 +60,46 @@ export const fetchTokenById = async (tokenId: number) => {
   const item: INFT = await fetchAndFormatToken(nftContract, marketContract, tokenId);
   return item;
 }
+
+export const fetchOwnTokens = async () => {
+  const provider = new ethers.providers.JsonRpcProvider();
+  const nftContract = new ethers.Contract(nftaddress, NFT.abi, provider);
+  const marketContract = new ethers.Contract(nftmarketaddress, Market.abi, provider);
+  const ownTokenIds = await getOwnTokenIds()
+
+  const items: INFT[] = await Promise.all(
+    ownTokenIds.map(async (tokenId: number) => {
+      const item = await fetchAndFormatToken(nftContract, marketContract, tokenId);
+      return item;
+    }),
+  );
+  return items;
+
+}
+
+export const getOwnerAddress = async () => {
+  const web3Modal = new Web3Modal();
+  const connection = await web3Modal.connect();
+  const provider = new ethers.providers.Web3Provider(connection);
+  const signer = provider.getSigner();
+  return await signer.getAddress()
+}
+
+const getOwnTokenIds = async () => {
+  const web3Modal = new Web3Modal();
+  const connection = await web3Modal.connect();
+  const provider = new ethers.providers.Web3Provider(connection);
+  const signer = provider.getSigner();
+  const nftContract = new ethers.Contract(nftaddress, NFT.abi, provider);
+
+  const ownerAddress = await signer.getAddress()
+  const balance = (await nftContract.balanceOf(ownerAddress)).toNumber();
+
+  const ownTokenIds = await Promise.all(
+    Array.from(new Array(balance)).map((_, i) => i).map(async (v, index) => {
+      const ownTokenId = await nftContract.tokenOfOwnerByIndex(ownerAddress, index);
+      return ownTokenId.toNumber();
+    })
+  )
+  return ownTokenIds;
+}
